0c75fc72ecb67fed9431fcb9299948311dab75cf
wwpark <wwpark@aimmeddtx>
2022-09-16 12:56:31 +0900

    마지막 백업 집에서 열심히해줘

--- routes/user/user-controller.js
+++ routes/user/user-controller.js
@@ -1,18 +1,56 @@
 var db = require('../../models');
-var sequelize = require('sequelize');
 const logger = require('../../config/logger');
+
+exports.findOverleb = async function (req, res, next) {
+    var { id } = req.body;
+    var result = {};
+    var statusNum;
+
+    try {
+        var hasId = await db.user.findOne({
+            where: { id: id },
+        });
+        if (hasId !== null) {
+            statusNum = 205;
+            result.status = 0;
+            result.message = '중복입니다';
+        } else {
+
+        }
+
+        res.status(statusNum).json(result);
+    } catch (error) {
+        next(error);
+    }
+};
 
 exports.join = async function (req, res, next) {
     var { id, password, name, birthday } = req.body;
+    var result = {};
+    var statusNum;
 
     try {
-        var data = await db.user.create({
-            id: id,
-            password: password,
-            name: name,
-            birthday: birthday,
+        var hasData = await db.user.findOne({
+            where: { id: id },
         });
-        res.json({ good: 'good' });
+        if (hasData !== null) {
+            db.user.create({
+                id: id,
+                password: password,
+                name: name,
+                birthday: birthday,
+            });
+
+            statusNum = 200;
+            result.status = 1;
+            result.message = '가입완료';
+        } else {
+            statusNum = ;
+            result.status = 0;
+            result.message = '아이디 중복인데 어떻게 들어왔냐...';
+        }
+
+        res.status(statusNum).json(result);
     } catch (error) {
         next(error);
     }
